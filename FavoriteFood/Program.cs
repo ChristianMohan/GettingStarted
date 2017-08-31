85ce7396ce943bd8705e25229f90a9982a4871de
Christian John Mohan <christianjmohan@gmail.com>
2017-08-31 10:29:11 -0400

    FavFood
    Version 0.9
    -added Pizza

---
+++ FavoriteFood/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is you favorite food?");
+            var favFood = Console.ReadLine();
+            {
+                if (favFood = "Pizza") ;
+                    Console.WriteLine("Wrong!  That is a terrible choice and you should feel bad about yourself.   Your favorite should be pizza!");
+                else
+                    Console.WriteLine("Most excellent and correct!  Pizza is indeed one of the best foods ever put on this earth.");
+            }
+        }
+
+    }
+}
