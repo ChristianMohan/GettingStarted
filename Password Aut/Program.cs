c9edcfc9052e7054d62a2880e5c0fc89c8100c2f
ChristianMohan <christianjmohan@gmail.com>
2017-09-06 10:30:13 -0400

    Password aut
    Version 0.1 Beta

---
+++ Password Aut/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Aut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Username");
+            var user = Console.ReadLine();
+            Console.WriteLine("Enter the Password");
+            var pass = Console.ReadLine();
+
+            if (pass == "letmein" && user == "root")
+            {
+                Console.WriteLine("Username and password authenticated.. you may proceed");
+            }
+
+
+        }
+    }
+}
