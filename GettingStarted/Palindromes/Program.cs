44c8727a24dd0225c5d62d5066d289e9da986b84
ChristianMohan <christianjmohan@gmail.com>
2017-09-12 11:16:40 -0400

    Palindrome v 0.9
    -bug where palindrome is alwasy not a palindrome

---
+++ GettingStarted/Palindromes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a phrase: ");
+                var word = Console.ReadLine();
+            char[] arr = word.ToCharArray();
+            Array.Reverse(arr);
+           // Console.WriteLine(arr);
+            char[] arr2 = word.ToCharArray();
+           // Console.WriteLine(arr2);
+            if (arr == arr2)
+            {
+                Console.WriteLine("/" + word + "/" + " is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("/" + word + "/" + " is not a palindrome");
+            }
+            Console.WriteLine("hello");
+            
+        }
+    }
+}
