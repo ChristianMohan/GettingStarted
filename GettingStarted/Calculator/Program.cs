8ae1ee2c00fea4a5d4788abb89c92cc82fec8432
ChristianMohan <christianjmohan@gmail.com>
2017-09-12 11:32:21 -0400

    Calculator v .9
    -fixed some bugs

---
+++ GettingStarted/Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an expression: ");
+            var math1 = Console.ReadLine();
+
+            math1 = math1.Replace(" ", String.Empty);
+            int equation = Convert.ToInt32(math1);
+            Console.WriteLine(math1);
+          //  int first = Convert.ToInt32(math1.Substring(0, 1));
+          //  int second = Convert.ToInt32(math1.Substring(1, 2));
+
+        }
+    }
+}
