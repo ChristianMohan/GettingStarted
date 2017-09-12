d521e19e2520ad1d9afc1603468eed49aebfad49
ChristianMohan <christianjmohan@gmail.com>
2017-09-12 10:46:05 -0400

    Grades v 1.0
    -fixed the grades

---
+++ Grade Match/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Match
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the grade");
+
+            var answer = Console.ReadLine();
+
+            if(answer == "e")
+            {
+                Console.WriteLine("You have chosen : excellent");
+            }
+            else if (answer == "v")
+            {
+                Console.WriteLine("You have chosen : very good");
+            }
+            else if (answer == "g")
+            {
+                Console.WriteLine("You have chosen : good");
+            }
+            else if (answer == "a")
+            {
+                Console.WriteLine("You have chosen : average");
+            }
+            else if (answer == "f")
+            {
+                Console.WriteLine("You have chosen : fail");
+            }
+
+        }
+    }
+}
